bc4269541127bfc6881efebce229249e6364ca3a
Eduardo Fermiano <e.fermiano.rocha@avanade.com>
2018-06-21 21:23:15 -0300

    Insert dos livros

--- routers/produtos.js
+++ routers/produtos.js
@@ -3,7 +3,7 @@ const connectionFactory = require('./../infra/connectionFactory')
 
 module.exports = (server) => { 
   
-  server.get('/produtos', (req, res) => {
+server.get('/produtos', (req, res) => {
   const connection = connectionFactory()
   const livrosDao = new LivrosDao(connection)
   
@@ -18,4 +18,26 @@ module.exports = (server) => {
   connection.end()
 
 })
+
+server.get('/produtos/form', (req, res) => {
+  res.render('produtos/form')
+})
+
+server.post('/produtos', (req, res) => {
+  const connection = connectionFactory()
+  const livro = req.body
+  const livrosDao = new LivrosDao(connection)
+  console.log(livro)
+
+  livrosDao.save(livro, (err) => {
+
+    if (err){
+      console.log(err)
+    }
+    
+    res.redirect('/produtos')
+  })
+
+})
+
 }
