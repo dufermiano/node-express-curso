b08e2f02b2ea224f9162c826b17b7bb784edd704
Eduardo Fermiano <e.fermiano.rocha@avanade.com>
2018-06-21 21:22:27 -0300

    Ajuste da tabela e inclusão de comandos

--- script_banco.sql
+++ script_banco.sql
@@ -3,14 +3,16 @@ CREATE DATABASE curso_node;
 USE curso_node;
 
 CREATE TABLE livros (id INT(11) NOT NULL AUTO_INCREMENT, 
-title VARCHAR(255) DEFAULT NULL,
-descricao TEXT, 
+titulo VARCHAR(255) DEFAULT NULL,
 preco DECIMAL (10, 2) DEFAULT NULL,
+descricao TEXT, 
 PRIMARY KEY (id));
 
-INSERT INTO livros (title, descricao, preco) VALUES ('Comecando com nodejs', 'Livro introdutorio de node', 39.90);
-INSERT INTO livros (title, descricao, preco) VALUES ('Comecando com express', 'Livro introdutorio de express', 39.90);
+INSERT INTO livros (titulo, descricao, preco) VALUES ('Comecando com nodejs', 'Livro introdutorio de node', 39.90);
+INSERT INTO livros (titulo, descricao, preco) VALUES ('Comecando com express', 'Livro introdutorio de express', 39.90);
 
 SELECT * FROM LIVROS;
+
+DROP TABLE LIVROS;
 
 UPDATE LIVROS SET PRECO = 39.9 WHERE id = 1
